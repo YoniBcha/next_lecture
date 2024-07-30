f8951c27098aa028ef927e4f80e4d7aebcb9b495
YoniBcha <yonasmillion29@gmail.com>
2024-07-30 13:12:57 +0300

    Catch-All Routes concept is done perfectly

--- src/app/docs/[...slug]/page.tsx
+++ src/app/docs/[...slug]/page.tsx
@@ -1,3 +1,13 @@
-export default function Docs() {
+export default function Docs({params}:{
+  params: {slug: string[]}
+}) {
+  
+  if(params.slug.length === 2){
+    return <h1>
+      Viewing Document for {params.slug[0]} and {params.slug[1]}
+    </h1>
+  }else if(params.slug.length === 1){
+    return <h1>Viewing Documentation for {params.slug[0]}</h1>
+  }
   return <h1>this is the home page for the documentation</h1>;
 }
