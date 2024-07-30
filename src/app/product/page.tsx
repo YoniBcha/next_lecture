a07f072c40f593deb6a726f169f9a2a6f9a90ac9
YoniBcha <yonasmillion29@gmail.com>
2024-07-30 16:47:40 +0300

    next Link is done

--- src/app/product/page.tsx
+++ src/app/product/page.tsx
@@ -1,12 +1,27 @@
+import Link from "next/link";
+
 export default function ProductList() {
+  const productId = 100;
   return (
     <>
+      <Link href="/">Home</Link>
       <h1>Product List</h1>
-      <h2>Product 1</h2>
-      <h2>Product 2</h2>
-      <h2>Product 3</h2>
-      <h2>Product 4</h2>
+      <h2>
+        {" "}
+        <Link href="product/1">Product 1</Link>
+      </h2>
+      <h2>
+        <Link href="product/2">Product 2</Link>
+      </h2>
+      <h2>
+        <Link href="product/3">Product 3</Link>
+      </h2>
+      <h2>
+        <Link href="product/4">Product 4</Link>
+      </h2>
+      <h2>
+        <Link href={`product/${productId}`}>Product {productId}</Link>
+      </h2>
     </>
   );
 }
- 
