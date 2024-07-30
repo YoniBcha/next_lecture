dcf270f105c9a2c0cfdd607cbf15686338da98fb
YoniBcha <yonasmillion29@gmail.com>
2024-07-30 15:52:55 +0300

    this part is about next metadata

--- src/app/product/[productId]/page.tsx
+++ src/app/product/[productId]/page.tsx
@@ -1,7 +1,15 @@
-export default function ProductDetail({
-  params,
-}: {
+import { Metadata } from "next";
+
+type Props = {
   params: { productId: string };
-}) {
+};
+
+export const generateMetadata = ({ params }: Props): Metadata => {
+  return {
+    title: `Product ${params.productId}`,
+  };
+};
+
+export default function ProductDetail({ params }: Props) {
   return <h1>About Product {params.productId} Detail</h1>;
 }
