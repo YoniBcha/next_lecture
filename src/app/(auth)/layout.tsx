e8eef217c6230827752ca16d278280d75a4f85f1
YoniBcha <yonasmillion29@gmail.com>
2024-07-31 08:29:15 +0300

    styling of active link

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,4 +1,8 @@
+"use client";
 import Link from "next/link";
+//this import is for to style active link
+import { usePathname } from "next/navigation";
+import "./style.css";
 
 export default function AuthLayout({
   children,
@@ -10,11 +14,18 @@ export default function AuthLayout({
     { name: "login", href: "/login" },
     { name: "forgotpassword", href: "/forgotpassword" },
   ];
+  const pathname = usePathname();
+
   return (
     <div>
       {navLinks.map((link) => {
+        const isActive = pathname.startsWith(link.href);
         return (
-          <Link href={link.href} key={link.name}>
+          <Link
+            href={link.href}
+            key={link.name}
+            className={isActive ? "font-bold mr-4" : "text-blue-500 mr-4"}
+          >
             {link.name}
           </Link>
         );
