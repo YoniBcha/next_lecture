6b85496d5f617c4bdbe2784f613475e0bcb41a63
YoniBcha <yonasmillion29@gmail.com>
2024-07-31 09:58:10 +0300

    error page in next

--- src/app/product/[productId]/reviews/[reviewId]/page.tsx
+++ src/app/product/[productId]/reviews/[reviewId]/page.tsx
@@ -1,11 +1,22 @@
+"use client";
+
 import { notFound } from "next/navigation";
+
+function getRandomInt(count: number) {
+  return Math.floor(Math.random() * count);
+}
 
 export default function ProductReview({
   params,
 }: {
   params: { reviewId: string; productId: string };
 }) {
-  if (parseInt(params.reviewId)>= 1000) {
+  const random = getRandomInt(2);
+  if (random === 1) {
+    throw new Error("Error Loading Review");
+  }
+
+  if (parseInt(params.reviewId) >= 1000) {
     return notFound();
   }
   return (
