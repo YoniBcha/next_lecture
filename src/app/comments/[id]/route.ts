f36ca784751d3346453396ccfc5920c4eb8104ad
YoniBcha <yonasmillion29@gmail.com>
2024-08-01 10:36:42 +0300

    curud operation are done

--- src/app/comments/[id]/route.ts
+++ src/app/comments/[id]/route.ts
@@ -9,3 +9,28 @@ export async function GET(
   );
   return Response.json(comment);
 }
+
+export async function PATCH(
+  request: Request,
+  { params }: { params: { id: string } }
+) {
+  const body = await request.json();
+  const { text } = body;
+  const index = comments.findIndex(
+    (commnet) => commnet.id === parseInt(params.id)
+  );
+  comments[index].text = text;
+  return Response.json(comments[index]);
+}
+
+export async function DELETE(
+  request: Request,
+  { params }: { params: { id: string } }
+) {
+  const index = comments.findIndex(
+    (commnet) => commnet.id === parseInt(params.id)
+  );
+  const deletedComments = comments[index];
+  comments.splice(index, 1);
+  return Response.json(deletedComments);
+}
